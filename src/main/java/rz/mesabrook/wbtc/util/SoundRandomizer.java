b519f42f0afd2c5180ffe6c895f1e9653e4d856d
RavenholmZombie <supersoftware1234@gmail.com>
2021-05-26 20:16:07 -0400

    Update SoundRandomizer.java

--- src/main/java/rz/mesabrook/wbtc/util/SoundRandomizer.java
+++ src/main/java/rz/mesabrook/wbtc/util/SoundRandomizer.java
@@ -5,6 +5,10 @@ import java.util.Random;
 import net.minecraft.util.SoundEvent;
 import rz.mesabrook.wbtc.init.SoundInit;
 
+/**
+ * Uses a randomizer to randomly select a sound from Immersibrook's custom sounds and then feeds
+ * its ID into the packet-based sound system.
+ */
 public class SoundRandomizer 
 {
 	public static String result;
