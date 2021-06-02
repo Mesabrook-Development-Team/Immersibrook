e9555d7a8f582c84d0fbdfdd9261087c8ad49c95
RavenholmZombie <supersoftware1234@gmail.com>
2021-06-02 19:40:40 -0400

    Smooth Birch Wood Ceiling Blocks

--- src/main/java/rz/mesabrook/wbtc/client/category/CategoryCeiling.java
+++ src/main/java/rz/mesabrook/wbtc/client/category/CategoryCeiling.java
@@ -165,6 +165,80 @@ public class CategoryCeiling extends AbstractCategory
 		add(ModBlocks.SO_PANEL_WOOL_GREEN);
 		add(ModBlocks.SO_PANEL_WOOL_RED);
 		add(ModBlocks.SO_PANEL_WOOL_BLACK);
+
+		/**
+		 * Smooth Birch Wood Ceiling Panel Blocks
+		 */
+		// Laminate Wood Panels
+		add(ModBlocks.SB_PANEL_OAK_L);
+		add(ModBlocks.SB_PANEL_SPRUCE_L);
+		add(ModBlocks.SB_PANEL_BIRCH_L);
+		add(ModBlocks.SB_PANEL_FLOOR_JUNGLE_L);
+		add(ModBlocks.SB_PANEL_ACACIA_L);
+		add(ModBlocks.SB_PANEL_DARK_OAK_L);
+
+		// Checkerboard Panels
+		add(ModBlocks.SB_PANEL_CHECKERBOARD);
+		add(ModBlocks.SB_PANEL_CHECKERBOARD_OLD);
+
+		// Floor Tile Panels
+		add(ModBlocks.SB_PANEL_TILE_1);
+		add(ModBlocks.SB_PANEL_TILE_2);
+		add(ModBlocks.SB_PANEL_TILE_3);
+		add(ModBlocks.SB_PANEL_TILE_4);
+		add(ModBlocks.SB_PANEL_TILE_5);
+		add(ModBlocks.SB_PANEL_TILE_6);
+		add(ModBlocks.SB_PANEL_TILE_7);
+		add(ModBlocks.SB_PANEL_TILE_8);
+		add(ModBlocks.SB_PANEL_TILE_9);
+		add(ModBlocks.SB_PANEL_TILE_10);
+		add(ModBlocks.SB_PANEL_TILE_11);
+		add(ModBlocks.SB_PANEL_TILE_12);
+		add(ModBlocks.SB_PANEL_TILE_13);
+		add(ModBlocks.SB_PANEL_TILE_14);
+		add(ModBlocks.SB_PANEL_TILE_15);
+		add(ModBlocks.SB_PANEL_TILE_16);
+		add(ModBlocks.SB_PANEL_TILE_17);
+		add(ModBlocks.SB_PANEL_TILE_18);
+		add(ModBlocks.SB_PANEL_TILE_19);
+		add(ModBlocks.SB_PANEL_TILE_20);
+		add(ModBlocks.SB_PANEL_TILE_21);
+		add(ModBlocks.SB_PANEL_TILE_22);
+		add(ModBlocks.SB_PANEL_TILE_23);
+		add(ModBlocks.SB_PANEL_TILE_24);
+		add(ModBlocks.SB_PANEL_TILE_25);
+		add(ModBlocks.SB_PANEL_TILE_26);
+		add(ModBlocks.SB_PANEL_TILE_27);
+
+		// Commercial Kitchen Tile Panels
+		add(ModBlocks.SB_PANEL_KITCHEN_1);
+		add(ModBlocks.SB_PANEL_KITCHEN_2);
+		add(ModBlocks.SB_PANEL_DRAIN_1);
+		add(ModBlocks.SB_PANEL_DRAIN_2);
+
+		// Industrial Tile Panels
+		add(ModBlocks.SB_PANEL_DANGER_1);
+		add(ModBlocks.SB_PANEL_DANGER_2);
+		add(ModBlocks.SB_PANEL_DANGER_3);
+		add(ModBlocks.SB_PANEL_DANGER_4);
+
+		// Wool Panels
+		add(ModBlocks.SB_PANEL_WOOL_WHITE);
+		add(ModBlocks.SB_PANEL_WOOL_ORANGE);
+		add(ModBlocks.SB_PANEL_WOOL_MAGENTA);
+		add(ModBlocks.SB_PANEL_WOOL_LBLUE);
+		add(ModBlocks.SB_PANEL_WOOL_YELLOW);
+		add(ModBlocks.SB_PANEL_WOOL_LIME);
+		add(ModBlocks.SB_PANEL_WOOL_PINK);
+		add(ModBlocks.SB_PANEL_WOOL_GRAY);
+		add(ModBlocks.SB_PANEL_WOOL_SILVER);
+		add(ModBlocks.SB_PANEL_WOOL_CYAN);
+		add(ModBlocks.SB_PANEL_WOOL_PURPLE);
+		add(ModBlocks.SB_PANEL_WOOL_BLUE);
+		add(ModBlocks.SB_PANEL_WOOL_BROWN);
+		add(ModBlocks.SB_PANEL_WOOL_GREEN);
+		add(ModBlocks.SB_PANEL_WOOL_RED);
+		add(ModBlocks.SB_PANEL_WOOL_BLACK);
 	}
 
 }
