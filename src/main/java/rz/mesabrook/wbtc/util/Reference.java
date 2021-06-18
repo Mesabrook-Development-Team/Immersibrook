f57fc8f17f4038ec8a9f1742cc062aad29c84d87
RavenholmZombie <supersoftware1234@gmail.com>
2021-06-18 11:21:05 -0400

    Update Changelog URL

--- src/main/java/rz/mesabrook/wbtc/util/Reference.java
+++ src/main/java/rz/mesabrook/wbtc/util/Reference.java
@@ -7,7 +7,7 @@ public class Reference
 	public static final String AUTHOR = "RavenholmZombie, CSX8600";
 	public static final String VERSION = "0.0.7.2";
 	public static final String BUILD_DATE = "TBD";
-	public static final String CHANGELOG = "https://bit.ly/3g3xgVm";
+	public static final String CHANGELOG = "https://bit.ly/3cQTojY";
 	
 	public static final String CLIENT = "rz.mesabrook.wbtc.proxy.ClientProxy";
 	public static final String SERVER = "rz.mesabrook.wbtc.proxy.CommonProxy";
