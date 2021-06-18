ec5846e812a5128081e8fa1bdcb0373350763143
RavenholmZombie <supersoftware1234@gmail.com>
2021-06-18 16:06:37 -0400

    Yee

--- src/main/java/rz/mesabrook/wbtc/util/Reference.java
+++ src/main/java/rz/mesabrook/wbtc/util/Reference.java
@@ -6,7 +6,7 @@ public class Reference
 	public static final String MODNAME = "Immersibrook";
 	public static final String AUTHOR = "RavenholmZombie, CSX8600";
 	public static final String VERSION = "0.0.7.2";
-	public static final String BUILD_DATE = "TBD";
+	public static final String BUILD_DATE = "18 May 2021";
 	public static final String CHANGELOG = "https://bit.ly/3cQTojY";
 	
 	public static final String CLIENT = "rz.mesabrook.wbtc.proxy.ClientProxy";
