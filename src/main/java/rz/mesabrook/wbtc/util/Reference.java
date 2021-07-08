4016251e390bac4ea1dcc6cf0f363f9c4a79918e
RavenholmZombie <supersoftware1234@gmail.com>
2021-07-08 13:25:18 -0400

    Start of 1.0.0

--- src/main/java/rz/mesabrook/wbtc/util/Reference.java
+++ src/main/java/rz/mesabrook/wbtc/util/Reference.java
@@ -4,8 +4,8 @@ public class Reference
 {
 	public static final String MODID = "wbtc";
 	public static final String MODNAME = "Immersibrook";
-	public static final String VERSION = "0.0.8";
-	public static final String BUILD_DATE = "4 July 2021";
+	public static final String VERSION = "1.0.0";
+	public static final String BUILD_DATE = "2021 :wheezing:";
 	public static final String CHANGELOG = "https://bit.ly/35EtRX4";
 	public static final String UPDATE_URL = "https://raw.githubusercontent.com/RavenholmZombie/RavenholmZombie/main/update.json";
 	
