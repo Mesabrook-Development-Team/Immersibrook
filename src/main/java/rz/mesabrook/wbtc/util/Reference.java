0c39ee600c09dc0c9efc08b5c8d36ed1c738a368
RavenholmZombie <supersoftware1234@gmail.com>
2021-07-08 13:26:26 -0400

    Update Reference.java

--- src/main/java/rz/mesabrook/wbtc/util/Reference.java
+++ src/main/java/rz/mesabrook/wbtc/util/Reference.java
@@ -6,7 +6,7 @@ public class Reference
 	public static final String MODNAME = "Immersibrook";
 	public static final String VERSION = "1.0.0";
 	public static final String BUILD_DATE = "2021 :wheezing:";
-	public static final String CHANGELOG = "https://bit.ly/35EtRX4";
+	public static final String CHANGELOG = "https://bit.ly/3wpUwSn";
 	public static final String UPDATE_URL = "https://raw.githubusercontent.com/RavenholmZombie/RavenholmZombie/main/update.json";
 	
 	public static final String CLIENT = "rz.mesabrook.wbtc.proxy.ClientProxy";
