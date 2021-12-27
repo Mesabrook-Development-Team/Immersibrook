8a33756c3ca212b7fca8962e3c5d881be12ca750
RavenholmZombie <supersoftware1234@gmail.com>
2021-12-27 14:20:31 -0500

    Updated Changelog to include /talk

--- src/main/java/rz/mesabrook/wbtc/util/Reference.java
+++ src/main/java/rz/mesabrook/wbtc/util/Reference.java
@@ -7,8 +7,8 @@ public class Reference
 	public static final String MODID = "wbtc";
 	public static final String MODNAME = "Immersibrook";
 	public static final String UPDATE_NAME = "The Technology Update";
-	public static final String VERSION = "1.0.0";
-	public static final String BUILD_DATE = "Whenever :shrug:";
+	public static final String VERSION = "1.0.0-PR2";
+	public static final String BUILD_DATE = "Sometime in 2022";
 	public static final String CHANGELOG = "https://bit.ly/3wpUwSn";
 	public static final String UPDATE_URL = "https://raw.githubusercontent.com/RavenholmZombie/RavenholmZombie/main/update.json";
 	
