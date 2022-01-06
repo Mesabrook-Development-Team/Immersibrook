f9fba623691dc7121066a212fde3589f67a3513a
RavenholmZombie <supersoftware1234@gmail.com>
2022-01-06 17:45:36 -0500

    Update Reference.java

--- src/main/java/rz/mesabrook/wbtc/util/Reference.java
+++ src/main/java/rz/mesabrook/wbtc/util/Reference.java
@@ -6,10 +6,10 @@ public class Reference
 {
 	public static final String MODID = "wbtc";
 	public static final String MODNAME = "Immersibrook";
-	public static final String UPDATE_NAME = "The Technology Update";
+	public static final String UPDATE_NAME = "The Technology Update - Pre-Release 2";
 	public static final String VERSION = "1.0.0-PR2";
 	public static final String BUILD_DATE = "Sometime in 2022";
-	public static final String CHANGELOG = "https://bit.ly/3wpUwSn";
+	public static final String CHANGELOG = "https://bit.ly/3zxMSZm";
 	public static final String UPDATE_URL = "https://raw.githubusercontent.com/RavenholmZombie/RavenholmZombie/main/update.json";
 	
 	public static final String CLIENT = "rz.mesabrook.wbtc.proxy.ClientProxy";
