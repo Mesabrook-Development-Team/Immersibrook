62bb82ecb2087701c17ea04e4abd5ddb95423bcc
CSX8600 <cnwaj@hotmail.com>
2022-02-01 23:22:38 -0600

    Update max number of ringtones

--- src/main/java/com/mesabrook/ib/util/Reference.java
+++ src/main/java/com/mesabrook/ib/util/Reference.java
@@ -50,7 +50,7 @@ public class Reference
 	
 	public static final int MAX_PHONE_BACKGROUNDS = 14; // This is an INCLUSIVE number
 	public static final int MAX_CHAT_NOTIFICATIONS = 7; // This is an INCLUSIVE number
-	public static final int MAX_RINGTONES = 1; // This is an INCLUSIVE number
+	public static final int MAX_RINGTONES = 11; // This is an INCLUSIVE number
 
 	// Birthdays (Day and Month only - NO YEARS)
 	public static final int RZ_MONTH = 9;
