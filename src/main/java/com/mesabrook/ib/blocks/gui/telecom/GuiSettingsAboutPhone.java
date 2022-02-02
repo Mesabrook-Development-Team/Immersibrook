292a75dee9def26c38d399ec210d77c9bbd21754
RavenholmZombie <supersoftware1234@gmail.com>
2022-02-02 15:21:54 -0500

    Added extra details to the About Phone GUI

--- src/main/java/com/mesabrook/ib/blocks/gui/telecom/GuiSettingsAboutPhone.java
+++ src/main/java/com/mesabrook/ib/blocks/gui/telecom/GuiSettingsAboutPhone.java
@@ -3,6 +3,7 @@ package com.mesabrook.ib.blocks.gui.telecom;
 import java.io.IOException;
 
 import com.mesabrook.ib.net.telecom.FactoryResetPacket;
+import com.mesabrook.ib.util.Reference;
 import com.mesabrook.ib.util.handlers.PacketHandler;
 
 import net.minecraft.client.Minecraft;
@@ -31,7 +32,7 @@ public class GuiSettingsAboutPhone extends GuiPhoneBase {
 		super.initGui();
 
 		back = new LabelButton(4, INNER_X + 3, INNER_Y + 20, "<", 0xFFFFFF);
-		factoryReset = new MinedroidButton(1, INNER_X + 3, INNER_Y + 50, INNER_TEX_WIDTH - 6, "Factory Reset", 0xFFFFFF);
+		factoryReset = new MinedroidButton(1, INNER_X + 3, INNER_Y + 160, INNER_TEX_WIDTH - 6, "Factory Reset", 0xFF5733);
 		
 		buttonList.add(back);
 		buttonList.add(factoryReset);
@@ -42,10 +43,15 @@ public class GuiSettingsAboutPhone extends GuiPhoneBase {
 		super.doDraw(mouseX, mouseY, partialticks);
 		
 		fontRenderer.drawString("About Phone", INNER_X + 15, INNER_Y + 20, 0xFFFFFF);
-		
-		fontRenderer.drawString("Phone Number:", INNER_X + 3, INNER_Y + 36, 0x4444FF);
+
+		fontRenderer.drawString("Minedroid", INNER_X + 3, INNER_Y + 56, 0x33CEFF);
+		fontRenderer.drawString("Version " + Reference.MINEDROID_VERSION, INNER_X + 3, INNER_Y + 66, 0xFFFFFF);
+
+		fontRenderer.drawString("Phone Information", INNER_X + 3, INNER_Y + 106, 0xFFFFFF);
+
+		fontRenderer.drawString("Phone Number:", INNER_X + 3, INNER_Y + 116, 0x4444FF);
 		int stringWidth = fontRenderer.getStringWidth("Phone Number:");
-		fontRenderer.drawString(getFormattedPhoneNumber(phoneStackData.getPhoneNumberString()), INNER_X + 3 + stringWidth + 3, INNER_Y + 36, 0xFFFFFF);
+		fontRenderer.drawString(getFormattedPhoneNumber(phoneStackData.getPhoneNumberString()), INNER_X + 3 + stringWidth + 3, INNER_Y + 116, 0xFFFFFF);
 		
 		GlStateManager.color(1, 1, 1);
 	}
