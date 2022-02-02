0732b8ef9f1d48cad3c9d46c46ad4dc33afb114f
RavenholmZombie <supersoftware1234@gmail.com>
2022-02-02 15:21:42 -0500

    Added a version string for Minedroid to Reference

--- src/main/java/com/mesabrook/ib/util/Reference.java
+++ src/main/java/com/mesabrook/ib/util/Reference.java
@@ -8,6 +8,7 @@ public class Reference
 	public static final String MODNAME = "Immersibrook";
 	public static final String UPDATE_NAME = "The Technology Update - Pre-Release 2";
 	public static final String VERSION = "1.0.0-PR2";
+	public static final String MINEDROID_VERSION = "0.0.2";
 	public static final String BUILD_DATE = "Sometime in 2022";
 	public static final String CHANGELOG = "https://bit.ly/3zxMSZm";
 	public static final String UPDATE_URL = "https://raw.githubusercontent.com/RavenholmZombie/RavenholmZombie/main/update.json";
