710fd2eb908398030f73eed50a4aca0778d2e398
RavenholmZombie <supersoftware1234@gmail.com>
2022-02-16 17:39:07 -0500

    Removed unused string from Reference

--- src/main/java/com/mesabrook/ib/util/Reference.java
+++ src/main/java/com/mesabrook/ib/util/Reference.java
@@ -9,7 +9,6 @@ public class Reference
 	public static final String UPDATE_NAME = "The Technology Update - Pre-Release 2";
 	public static final String VERSION = "1.0.0-PR2.1";
 	public static final String MINEDROID_VERSION = "0.0.2a";
-	public static final String BUILD_DATE = "Sometime in 2022";
 	public static final String CHANGELOG = "https://bit.ly/3oTNHHS";
 	public static final String UPDATE_URL = "https://raw.githubusercontent.com/RavenholmZombie/RavenholmZombie/main/update.json";
 	
