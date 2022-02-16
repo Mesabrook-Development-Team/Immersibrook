ee850ab7eecc781e77bbbe9a877bdedf3cf0f421
RavenholmZombie <supersoftware1234@gmail.com>
2022-02-16 17:38:48 -0500

    Added tooltip to Manhole Hook item

--- src/main/java/com/mesabrook/ib/items/tools/ItemManholeHook.java
+++ src/main/java/com/mesabrook/ib/items/tools/ItemManholeHook.java
@@ -1,9 +1,23 @@
 package com.mesabrook.ib.items.tools;
 
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import com.mesabrook.ib.Main;
 import com.mesabrook.ib.init.ModItems;
 import com.mesabrook.ib.util.IHasModel;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.text.TextComponentTranslation;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+import javax.annotation.Nullable;
+import java.util.List;
 
 public class ItemManholeHook extends Item implements IHasModel
 {
@@ -22,5 +36,19 @@ public class ItemManholeHook extends Item implements IHasModel
     public void registerModels()
     {
         Main.proxy.registerItemRenderer(this, 0);
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void addInformation(ItemStack stack, @Nullable World world, List<String> tooltip, ITooltipFlag flag)
+    {
+        if(GuiScreen.isShiftKeyDown())
+        {
+            tooltip.add(TextFormatting.YELLOW + new TextComponentTranslation("im.tooltip.manhole.hook").getFormattedText());
+        }
+        else
+        {
+            tooltip.add(TextFormatting.WHITE + new TextComponentTranslation("im.tooltip.hidden").getFormattedText());
+        }
     }
 }
