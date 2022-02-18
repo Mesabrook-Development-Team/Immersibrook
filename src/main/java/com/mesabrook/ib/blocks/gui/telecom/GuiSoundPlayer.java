b1e8876c4588bb491e6e52fdc8a5b48b26b31d8c
RavenholmZombie <supersoftware1234@gmail.com>
2022-02-18 11:33:59 -0500

    Updated Sound Player app's UI

--- src/main/java/com/mesabrook/ib/blocks/gui/telecom/GuiSoundPlayer.java
+++ src/main/java/com/mesabrook/ib/blocks/gui/telecom/GuiSoundPlayer.java
@@ -12,17 +12,20 @@ import net.minecraft.client.resources.I18n;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.text.TextFormatting;
 
 import java.io.IOException;
 
 public class GuiSoundPlayer extends GuiPhoneBase
 {
     private String currentlyPlaying = I18n.format("ib.musicapp.statusdefault");
-    LabelButton playSound;
-    LabelButton reset;
-    LabelButton example;
+    MinedroidButton playSound;
+    MinedroidButton reset;
+    MinedroidButton example;
     GuiTextField modIDText;
     GuiTextField soundIDText;
+    GuiTextField volumeText;
+    GuiTextField pitchText;
 
     public GuiSoundPlayer(ItemStack phoneStack, EnumHand hand)
     {
@@ -40,15 +43,22 @@ public class GuiSoundPlayer extends GuiPhoneBase
     {
         super.initGui();
 
-        reset = new LabelButton(1, INNER_X + (INNER_TEX_WIDTH / 4), INNER_Y + INNER_TEX_HEIGHT - 33, "Reset", 0xFFFFFF);
-        playSound = new LabelButton(2, INNER_X + INNER_TEX_WIDTH - (INNER_TEX_WIDTH / 4), INNER_Y + INNER_TEX_HEIGHT - 33, "Play", 0xFFFFFF);
-        playSound.x = playSound.x - playSound.width;
-        example = new LabelButton(3, INNER_X + INNER_TEX_WIDTH - 89, INNER_Y + INNER_TEX_HEIGHT - 136, "Load Example", 0xFFFFFF);
+        int lowerControlsY = INNER_Y + INNER_TEX_HEIGHT - INNER_TEX_Y_OFFSET - 32;
+        reset = new MinedroidButton(1, INNER_X + 50, lowerControlsY - 20, 32, "Reset", 0xFFFFFF);
+        playSound = new MinedroidButton(2, INNER_X + 85, lowerControlsY - 20, 30, "Play", 0xFFFFFF);
+        example = new MinedroidButton(3, reset.x, lowerControlsY - 35, 65, "Example", 0xFFFFFF);
 
-        modIDText = new GuiTextField(10, fontRenderer, INNER_X + 60, INNER_Y + 50, 80, 10);
-        soundIDText = new GuiTextField(11, fontRenderer, INNER_X + 60, INNER_Y + 70, 80, 10);
+        modIDText = new GuiTextField(10, fontRenderer, INNER_X + 60, INNER_Y + 50, 85, 10);
+        soundIDText = new GuiTextField(11, fontRenderer, INNER_X + 60, INNER_Y + 70, 85, 10);
 
-        buttonList.addAll(ImmutableList.of(reset, playSound, example));
+        volumeText = new GuiTextField(12, fontRenderer, INNER_X + 60, INNER_Y + 90, 25, 10);
+        pitchText = new GuiTextField(13, fontRenderer, INNER_X + 120, INNER_Y + 90, 25, 10);
+
+        buttonList.addAll(ImmutableList.<GuiButton>builder()
+                .add(reset)
+                .add(playSound)
+                .add(example)
+                .build());
     }
 
     @Override
@@ -59,12 +69,16 @@ public class GuiSoundPlayer extends GuiPhoneBase
 
         fontRenderer.drawString("Mod ID:", INNER_X + 15, INNER_Y + 50, 0x4444FF);
         fontRenderer.drawString("Sound ID:", INNER_X + 3, INNER_Y + 70, 0x4444FF);
+        fontRenderer.drawString("  Volume:", INNER_X + 3, INNER_Y + 90, 0x4444FF);
+        fontRenderer.drawString("  Pitch:", INNER_X + 82, INNER_Y + 90, 0x4444FF);
 
-        drawCenteredString(fontRenderer, "Now Playing:", INNER_X + 80, INNER_Y + 120, 3395327);
-        drawCenteredString(fontRenderer, currentlyPlaying, INNER_X + 80, INNER_Y + 140, 3395327);
+        drawCenteredString(fontRenderer, TextFormatting.BOLD + "Now Playing:", INNER_X + 80, INNER_Y + 115, 3395327);
+        drawCenteredString(fontRenderer, TextFormatting.ITALIC + currentlyPlaying, INNER_X + 80, INNER_Y + 129, 3395327);
 
         modIDText.drawTextBox();
         soundIDText.drawTextBox();
+        volumeText.drawTextBox();
+        pitchText.drawTextBox();
 
         GlStateManager.color(1, 1, 1);
     }
@@ -75,6 +89,8 @@ public class GuiSoundPlayer extends GuiPhoneBase
         super.mouseClicked(mouseX, mouseY, mouseButton);
         modIDText.mouseClicked(mouseX, mouseY, mouseButton);
         soundIDText.mouseClicked(mouseX, mouseY, mouseButton);
+        volumeText.mouseClicked(mouseX, mouseY, mouseButton);
+        pitchText.mouseClicked(mouseX, mouseY, mouseButton);
     }
 
     @Override
@@ -83,6 +99,8 @@ public class GuiSoundPlayer extends GuiPhoneBase
         super.keyTyped(typedChar, keyCode);
         modIDText.textboxKeyTyped(typedChar, keyCode);
         soundIDText.textboxKeyTyped(typedChar, keyCode);
+        volumeText.textboxKeyTyped(typedChar, keyCode);
+        pitchText.textboxKeyTyped(typedChar, keyCode);
     }
 
     @Override
@@ -103,6 +121,20 @@ public class GuiSoundPlayer extends GuiPhoneBase
                 packet.pos = player.getPosition();
                 packet.modID = modIDText.getText();
                 packet.soundName = soundIDText.getText();
+
+                try
+                {
+                    packet.volume = Float.parseFloat(volumeText.getText());
+                    packet.pitch = Float.parseFloat(pitchText.getText());
+                }
+                catch(Exception ex)
+                {
+                    packet.volume = 1.0F;
+                    packet.pitch = 1.0F;
+                    volumeText.setText("1.0");
+                    pitchText.setText("1.0");
+                }
+
                 PacketHandler.INSTANCE.sendToServer(packet);
 
                 currentlyPlaying = ModUtils.truncator(packet.modID + ":" + packet.soundName, "...", INNER_TEX_WIDTH / 6);
@@ -119,6 +151,8 @@ public class GuiSoundPlayer extends GuiPhoneBase
         {
             modIDText.setText("minecraft");
             soundIDText.setText("record.chirp");
+            volumeText.setText("1.0");
+            pitchText.setText("1.0");
         }
     }
 }
