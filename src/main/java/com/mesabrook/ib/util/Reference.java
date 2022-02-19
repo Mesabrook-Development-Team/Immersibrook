598efc0960e6991a6955e2a045cff98146534129
RavenholmZombie <supersoftware1234@gmail.com>
2022-02-18 19:47:18 -0500

    New Wallpapers

--- src/main/java/com/mesabrook/ib/util/Reference.java
+++ src/main/java/com/mesabrook/ib/util/Reference.java
@@ -49,7 +49,7 @@ public class Reference
 	public static final String RING_TONE = "ringTone";
 	public static final String CONTACTS_NBTKEY = "contacts";
 	
-	public static final int MAX_PHONE_BACKGROUNDS = 15; // This is an INCLUSIVE number
+	public static final int MAX_PHONE_BACKGROUNDS = 17; // This is an INCLUSIVE number
 	public static final int MAX_CHAT_NOTIFICATIONS = 8; // This is an INCLUSIVE number
 	public static final int MAX_RINGTONES = 12; // This is an INCLUSIVE number
 
