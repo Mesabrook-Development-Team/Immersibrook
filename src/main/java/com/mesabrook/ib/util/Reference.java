7a93a693688f414425d45039611c365202d8fb38
RavenholmZombie <supersoftware1234@gmail.com>
2022-06-02 20:06:18 -0400

    Updated version info

--- src/main/java/com/mesabrook/ib/util/Reference.java
+++ src/main/java/com/mesabrook/ib/util/Reference.java
@@ -6,8 +6,9 @@ public class Reference
 {
 	public static final String MODID = "wbtc";
 	public static final String MODNAME = "Immersibrook";
-	public static final String UPDATE_NAME = "The Technology Update - Pre-Release 3";
-	public static final String VERSION = "1.0.0-PR3";
+	public static final String UPDATE_NAME = "The Technology Update";
+	public static final String MOTTO = "Слава Україні!";
+	public static final String VERSION = "1.0.0";
 	public static final String MINEDROID_VERSION = "0.0.4";
 	public static final String CHANGELOG = "https://bit.ly/3oTNHHS";
 	public static final String UPDATE_URL = "https://raw.githubusercontent.com/RavenholmZombie/RavenholmZombie/main/update.json";
