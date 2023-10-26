86b5e7e3fbd575f513f97b2fdabe704ab2382ff5
Chris <supersoftware1234@gmail.com>
2023-10-26 15:46:06 -0400

    Update TestApp.java

--- src/main/java/com/mesabrook/ib/cdm/apps/TestApp.java
+++ src/main/java/com/mesabrook/ib/cdm/apps/TestApp.java
@@ -1,14 +1,34 @@
 package com.mesabrook.ib.cdm.apps;
 
 import com.mrcrayfish.device.api.app.Application;
+import com.mrcrayfish.device.api.app.component.Button;
+import com.mrcrayfish.device.api.app.component.Label;
+import com.mrcrayfish.device.api.app.listener.ClickListener;
 import net.minecraft.nbt.NBTTagCompound;
+
+import java.awt.*;
 
 public class TestApp extends Application
 {
     @Override
     public void init()
     {
+        Label helloWorld = new Label("Hello World!", 3, 5);
+        Button helloButton = new Button(3, 20, "Hello!");
 
+        helloButton.setClickListener(new ClickListener()
+        {
+            @Override
+            public void onClick(int i, int i1, int i2)
+            {
+                helloWorld.setText("Button Clicked!");
+                helloWorld.setTextColor(Color.BLUE);
+            }
+        });
+
+
+        addComponent(helloWorld);
+        addComponent(helloButton);
     }
 
     @Override
