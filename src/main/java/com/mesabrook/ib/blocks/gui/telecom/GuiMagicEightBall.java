d800abf6b0c405cbc3b52bbea024e260d18d132b
Christopher <supersoftware1234@gmail.com>
2024-05-03 11:26:56 -0400

    Magic 8 Ball App

--- src/main/java/com/mesabrook/ib/blocks/gui/telecom/GuiMagicEightBall.java
+++ src/main/java/com/mesabrook/ib/blocks/gui/telecom/GuiMagicEightBall.java
@@ -7,10 +7,17 @@ import java.util.Random;
 
 import org.lwjgl.input.Keyboard;
 
+import com.mesabrook.ib.util.IndependentTimer;
+import com.mesabrook.ib.util.Reference;
+
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiTextField;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.init.SoundEvents;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraft.util.text.TextComponentTranslation;
 import net.minecraft.util.text.TextFormatting;
@@ -30,6 +37,7 @@ public class GuiMagicEightBall extends GuiPhoneBase
 	private int submitButtonY;
 	private int submitButtonWidth;
 	
+	private IndependentTimer timer;
 	
 	static 
 	{
@@ -58,20 +66,20 @@ public class GuiMagicEightBall extends GuiPhoneBase
 	public GuiMagicEightBall(ItemStack phoneStack, EnumHand hand) 
 	{
 		super(phoneStack, hand);
-		displayAnswer = "Ask me your questions.";
+		displayAnswer = "~Ask me your questions~";
 	}
 
 	@Override
 	protected String getInnerTextureFileName() 
 	{
-		return phoneStackData.getIconTheme() + "/app_screen_contacts.png";
+		return phoneStackData.getIconTheme() + "/app_screen_purple.png";
 	}
 	
 	public static String getRandomAnswer() 
 	{
         Random random = new Random();
         int index = random.nextInt(answers.size());
-        return displayAnswer = answers.get(index);
+        return displayAnswer = "~" + answers.get(index) + "~";
     }
 	
 	@Override
@@ -83,6 +91,8 @@ public class GuiMagicEightBall extends GuiPhoneBase
 		questionBox = new GuiTextField(1, fontRenderer, INNER_X + 5, INNER_Y + 165, 151, 10);
 		submit = new MinedroidButton(2, INNER_X + 61, lowerControlsY + 15, 40, "Ask", 0x000000);
 		buttonList.add(submit);
+		
+		Minecraft.getMinecraft().player.playSound(SoundEvents.ENTITY_EXPERIENCE_ORB_PICKUP, 1F, 1F);
 	}
 	
 	@Override
@@ -90,14 +100,22 @@ public class GuiMagicEightBall extends GuiPhoneBase
 	{
 		super.doDraw(mouseX, mouseY, partialticks);
 		fontRenderer.drawString(new TextComponentString("Magic 8 Ball").getFormattedText(), INNER_X + 5, INNER_Y + 20, 0xFFFFFF);
-		drawRect(INNER_X + 3, INNER_Y + 40, INNER_X + 159, INNER_Y + 150, 0xFFAAAAAA);
-        drawRect(INNER_X + 5, INNER_Y + 42, INNER_X + 157, INNER_Y + 148, 0xFF000000);
 		drawCenteredString(fontRenderer, new TextComponentString(TextFormatting.ITALIC + displayAnswer).getFormattedText(), INNER_X + 80, INNER_Y + 130, 0xFFFFFF);
 		
 		questionBox.drawTextBox();
 		
-		fontRenderer.drawString("Ask the Magic 8 Ball!", questionBox.x, questionBox.y - 10, 0x0b520a);
+		fontRenderer.drawString("Ask the Magic 8 Ball!", questionBox.x, questionBox.y - 10, 0xFFFFFF);
+		showBall();
+		
 	}
+	
+    private void showBall()
+    {
+    	Minecraft.getMinecraft().getTextureManager().bindTexture(new ResourceLocation(Reference.MODID, "textures/gui/telecom/" + phoneStackData.getIconTheme() + "/eightball.png"));
+    	GlStateManager.color(1, 1, 1);
+    	drawScaledCustomSizeModalRect(INNER_X + 45, INNER_Y + 50, 0, 0, 70, 70, 70, 70, 70, 70);
+    }
+    
 	
     @Override
     protected void mouseClicked(int mouseX, int mouseY, int mouseButton) throws IOException
@@ -122,11 +140,11 @@ public class GuiMagicEightBall extends GuiPhoneBase
 			if(questionBox.getText() != "")
 			{
 				getRandomAnswer();
-				questionBox.setText("");
+				Minecraft.getMinecraft().player.playSound(SoundEvents.ENTITY_PLAYER_LEVELUP, 1F, 1F);
 			}
 			else
 			{
-				displayAnswer = "Ask me your questions.";
+				displayAnswer = "~Ask me your questions~";
 			}
 		}
 	}
